535424c5f5482153f85470ac3c2ad9fc855c5a38
anthony keel <farmtireguy@gmail.com>
2021-05-03 11:40:05 -0500

    aded button functions new quote and tweet

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const quoteContainer = document.getElementById('quote-conatiner');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes =[];
 // show new quote
 function newQuote(){
     // to pick random quote from api quotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // check for blank author and replace 
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    }else {
+    authorText.textContent = quote.author;
+    }
+    // check quote length and apply appropiate sytle
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+        quoteText.textContent = quote.text;
 }
 
 // Get quotes from API 
@@ -18,5 +36,16 @@ async function getQuotes() {
         // handle errors
     }
 }
+
+// tweet a quote 
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+
+}
+
+// event listners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
     // On Load
     getQuotes();
